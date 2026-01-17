382ac6283525e6fc27c33c97a8e72dca1260a508
Bashmachok1982 <Bashmachok1982@gmail.com>
2026-01-17 02:57:37 +0200

    my homework 11

---
+++ src/js/render-functions.js
@@ -0,0 +1,63 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+if (!galleryEl) console.error('Элемент .gallery не найден');
+if (!loaderEl) console.error('Элемент .loader не найден');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  if (!images || images.length === 0 || !galleryEl) return;
+
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags = 'Изображение',
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${webformatURL}"
+            alt="${tags.replace(/"/g, '&quot;')}"  // экранируем кавычки в alt
+            loading="lazy"
+          />
+        </a>
+        <div class="info">
+          <p><b>Likes</b> ${likes}</p>
+          <p><b>Views</b> ${views}</p>
+          <p><b>Comments</b> ${comments}</p>
+          <p><b>Downloads</b> ${downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  if (galleryEl) galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  if (loaderEl) loaderEl.classList.add('is-visible');
+}
+
+export function hideLoader() {
+  if (loaderEl) loaderEl.classList.remove('is-visible');
+}
